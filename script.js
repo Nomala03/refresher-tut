a4a39aab33c17f60790feefbd619484adc6806a4
nomala03 <mbalimadonsela03@gmail.com>
2025-07-10 17:40:32 +0200

    added arrays and objects
    added arrays with array methods, removing elements etc

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ let unAssigned // declaration keyword, variable name == undefined
 //Methods: a function embedded on the datatypes in JS
 //console.log(surname.trim());
 if (surname.trim() === ''){ // triple = sign is used because it is more precise
-     console.log('surname is required');
+    // console.log('surname is required');
 } else {
-    console.log(surname);
+    //console.log(surname);
 }
 
 //property
-console.log(surname.length);
-isMale = false;
-console.log(isMale);
+//console.log(surname.length);
+//isMale = false;
+//console.log(isMale);
 
-console.log(ageAsString);
-console.log(ageAsString.charAt(0));
+//console.log(ageAsString);
+//console.log(ageAsString.charAt(0));
 
 
 // Arrays and Objects
@@ -34,8 +34,8 @@ let courses = ["Computer Science", "Biological Science", "Information Technology
     
 //index: start at 0, making the last element to be at index = courses.length - 1.
 //e.g., computer science is at position one but if we use the index fomular it will be index = 1 - 1 = 0. This Makes the position of the course "0".
-console.log(course[0]);
-console.log(courses[course.length - 1]);
+//console.log(courses[0]);
+//console.log(courses[courses.length - 2]);
 
  // brakets are used to open an object
 let csCourse = {
@@ -50,20 +50,21 @@ let csCourse = {
 }
 
 //How to update the course name:
-
-console.log(csCourse)
-
+//console.log(csCourse)
 csCourse.courseName = "Information Tech"
+
 // To add a module:
 csCourse.modules.push("Statistics")
 csCourse.modules[4] = "Mobile Computing"
-csCourse.modules[0] = csCourse.modules[0].trim()
+csCourse.modules[0] = csCourse.modules[0].trim() //on csCourse what do you want us to do ?
+
 //If we want to delete a key. we call the object and the key.
 delete csCourse.courseName
 console.log(csCourse);
 
 //looping to check for values in an array (or object) while not being sure which index something is in. 
-// e.g., If we want to check if the Computer Science course has Statistics
+// e.g., If we want to check if the Computer Science course has Statistics, you'd have to loop through (csCourse.modules) check through if statements. 
+
 
 
  
